dddeccef94dc5bd011c97cd8c8a9cdb7921dde5c
Ajeet Kumar Chouksey <107052100+ajeetchouksey@users.noreply.github.com>
2024-12-16 13:52:44 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "name" {
   description = "The name of the resource group"
   value       = azurerm_resource_group.this.name
 }
+output "location" {
+ description = "The location of the resource group"
+  value       = azurerm_resource_group.this.location
+}
 
 output "resource" {
   description = "This is the full output for the resource group."
