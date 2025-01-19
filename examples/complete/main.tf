2e039c131230236fd54a704ac5cd187d2102986a
Jared Holgate <jaredholgate@microsoft.com>
2025-01-19 17:10:59 +0000

    Fix azurerm version in test

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71, <= 3.108.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
 terraform {
   required_version = ">= 1.5.2"
   required_providers {
-    # TODO: Ensure all required providers are listed here.
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71, < 5.0.0"
