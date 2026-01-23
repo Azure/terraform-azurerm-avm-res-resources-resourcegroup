6e3f8038e9e5834850561599e454de94de90a2e4
Matt Garner <117724543+MattGarnerAWR@users.noreply.github.com>
2026-01-23 15:25:00 +0000

    add resource group location to outputs (#112)

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "resource_id" {
   description = "The resource Id of the resource group"
   value       = azurerm_resource_group.this.id
 }
+
+output "location" {
+  description = "The location of the resource group"
+  value       = azurerm_resource_group.this.location
+}
